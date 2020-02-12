a7ca24327d94f5aa9165a65770293af0c88d14f9
qiuyiy <60947711+qiuyiy@users.noreply.github.com>
2020-02-11 22:13:24 -0500

    Add files via upload

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -3,6 +3,7 @@ import cc_dat_utils
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
 
-
+cc_level_pack = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
+print(cc_level_pack)
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
 #print the resulting data
